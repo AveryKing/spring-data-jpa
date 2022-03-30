0d797d7c4c7f58f50dc84f9c492a0ccb0084f19d
Avery King <kavery086@gmail.com>
2022-03-30 16:56:15 -0400

    ``RequestMapping`` added to ``StudentController`` (``/api/students``)

--- src/main/java/com/avery/springboot/student/StudentController.java
+++ src/main/java/com/avery/springboot/student/StudentController.java
@@ -1,11 +1,15 @@
 package com.avery.springboot.student;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping(
+        path = "api/students"
+)
 public class StudentController {
 
     @GetMapping
